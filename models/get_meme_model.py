dd76f984dd4dfa4a65f8eab52fb40bd2d204b2a7
Evgenii <teror1716@yandex.ru>
2024-03-30 16:38:33 +0300

    Fix problem with the model

--- models/get_meme_model.py
+++ models/get_meme_model.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel
 
 class Data(BaseModel):
     id: Optional[int] = None
-    info: Optional[Dict[str, Union[List[str], str]]] = None
-    tags: Optional[List[str]] = None
+    info: Optional[Dict[str, Union[List[str], str, int]]] = None
+    tags: Optional[List[Union[str, int]]] = None
     text: Optional[str] = None
     updated_by: Optional[str] = None
     url: Optional[str] = None
